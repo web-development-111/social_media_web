a9135982219379dea6f3f2e14324f2a1abbcb82d
mahasan24 <mahasan24@students.oamk.fi>
2025-03-24 07:34:28 +0545

    init

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Connectly",
+  description: "A modern social media app powered by Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <h1>Welcome to web development Project</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <div className="space-y-6 p-4">
+      <p>Home Page</p>
+    </div>
+  );
+}
